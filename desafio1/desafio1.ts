1c10801745b7671879c1f9691928897abd54aa4b
Everton <thi.ger21@gmail.com>
2022-06-28 09:51:44 -0300

    Respostas dos 4 desafios

---
+++ desafio1/desafio1.ts
@@ -0,0 +1,12 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+// let employee = {};
+// employee.code = 10;
+// employee.name = "John"
+
+//RESPOSTA//
+
+let Funcionario: {code: number, name: string} = {
+    code: 10,
+    name: "Jhon"
+}
